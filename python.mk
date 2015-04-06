90c823c5b9c62d1de1d9f117c9128255e536d5ac
wuyadong <wyd.npu@gmail.com>
2015-04-06 16:54:45 +0800

    Create python.mk

---
+++ python.mk
@@ -0,0 +1,9 @@
+#Python日志
+
+centos 安装python3
+stackoverflow -- http://stackoverflow.com/questions/8087184/installing-python3-on-rhel
+命令行:
+sudo yum install http://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-13.ius.centos7.noarch.rpm
+sudo yum search python3
+sudo yum install python34
+
